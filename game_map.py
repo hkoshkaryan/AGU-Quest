6a32bd78a1d361361bbdd1d080989bf281a800a1
hkoshkaryan <96400436+hkoshkaryan@users.noreply.github.com>
2021-12-20 21:25:32 -0800

    Set up dungeon room generation

--- game_map.py
+++ game_map.py
@@ -7,10 +7,9 @@ import tile_types
 class GameMap:
     def __init__(self, width: int, height: int):
         self.width, self.height=width, height
-        self.tiles = np.full((width, height), fill_value=tile_types.floor, order="F")
+        self.tiles = np.full((width, height), fill_value=tile_types.wall, order="F")
         """Line above creates a 2d array, fill with the same value of tile_types.floor"""
 
-        self.tiles[30:33, 22] = tile_types.wall
 
     def in_bounds(self, x: int, y: int) -> bool:
         """Return True if x and y are inside of the bounds of the map."""
